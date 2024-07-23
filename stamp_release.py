80897215082d4231441cc740be7ee7d5d87375ab
Bruce Campbell <yakman2020@gmail.com>
2024-07-23 14:35:36 -0700

    Updated build version number to fit in 16 bit values

--- stamp_release.py
+++ stamp_release.py
@@ -2,12 +2,15 @@
 
 from datetime import datetime
 
-v = open("_version.h", "r")
+v = open("version.h", "r")
 cur_time = datetime.utcnow()
 lines = v.readlines()
 for i, line in enumerate(lines):
-    if "MWLUSB_BUILD_VERSION" in line:
-        datestr = cur_time.strftime("%y%m%d%H")
+    if "MWLUSB_MINOR_VERSION" in line:
+        datestr = cur_time.strftime("%Y")
+        lines[i] = "#define MWLUSB_MINOR_VERSION    "+datestr+"\n"
+    elif "MWLUSB_BUILD_VERSION" in line:
+        datestr = cur_time.strftime("%m%d")
         lines[i] = "#define MWLUSB_BUILD_VERSION    "+datestr
 
 v = open("version.h.new", "w")
